49341245939046712f0577189fe2304f24355f4c
Andrey Groza <grozwalker@gmail.com>
2023-07-10 12:46:16 +0000

    change pipeline path

--- airbyte-integrations/connectors/source-amocrm/source_amocrm/source.py
+++ airbyte-integrations/connectors/source-amocrm/source_amocrm/source.py
@@ -61,7 +61,7 @@ class Pipelines(AmocrmStream):
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
-        return "pipelines"
+        return "leads/pipelines"
 
 # Source
 class SourceAmocrm(AbstractSource):
