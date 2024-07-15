9f1162349b04db9ae4deeddefa83a834f6e279c6
Andrey Groza <grozwalker@gmail.com>
2024-07-15 09:49:47 +0000

    fix va ads source

--- airbyte-integrations/connectors/source-vk-ads-2/source_vk_ads_2/source.py
+++ airbyte-integrations/connectors/source-vk-ads-2/source_vk_ads_2/source.py
@@ -7,6 +7,8 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
+from requests.auth import AuthBase
+from airbyte_cdk.sources.streams.http.auth.core import HttpAuthenticator
 import pendulum
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
@@ -14,6 +16,7 @@ from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
 
 URL_BASE: str = "https://ads.vk.com/api/v2/"
+
 
 # Basic full refresh stream
 class VkAds_2Stream(HttpStream, ABC):
@@ -32,7 +35,10 @@ class VkAds_2Stream(HttpStream, ABC):
         return None
 
     def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         params = {
             "limit": self.limit
@@ -45,46 +51,69 @@ class VkAds_2Stream(HttpStream, ABC):
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         return response.json().get('items')
 
+
 class AdPlans(VkAds_2Stream):
     primary_key = "id"
 
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "ad_plans.json?fields=id,created,name,status,ad_object_type"
+
 
 class AdGroups(VkAds_2Stream):
     primary_key = "id"
 
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "ad_groups.json?fields=id,created,name,status,ad_plan_id,package_id,utm"
+
 
 class Banners(VkAds_2Stream):
     primary_key = "id"
 
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "banners.json?fields=id,name,status,created,ad_group_id,urls,moderation_status,content"
+
 
 class Statistics(VkAds_2Stream):
     primary_key = "id"
 
+    def __init__(self, authenticator: AuthBase, config: Mapping[str, Any]):
+        self.config = config
+        super().__init__(authenticator)
+
     def path(
-        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> str:
         return "statistics/banners/day.json"
 
     def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, any] = None,
+        next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         return {
             "date_from": f"{self.config['replication_start_date']}",
-            # "date_from": "2023-07-12",
             "date_to": f"{pendulum.today().date()}"
         }
+
 
 # Source
 class SourceVkAds_2(AbstractSource):
@@ -127,5 +156,5 @@ class SourceVkAds_2(AbstractSource):
             AdPlans(authenticator=oauth),
             AdGroups(authenticator=oauth),
             Banners(authenticator=oauth),
-            Statistics(authenticator=oauth),
+            Statistics(authenticator=oauth, config=config),
         ]
